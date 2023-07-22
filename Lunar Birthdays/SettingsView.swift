4f42e6209c9a3a717cf6853ad53b043d1c08c72d
Johnny Jiang <johnnyj2608@gmail.com>
2023-07-21 22:22:50 -0400

    new views

---
+++ Lunar Birthdays/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  Settings.swift
+//  Lunar Birthdays
+//
+//  Created by Johnny Jiang on 7/21/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
