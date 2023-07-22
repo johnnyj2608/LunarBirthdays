0a4f538edaf78c144c3f9b45889211a544b483ca
Johnny Jiang <johnnyjiang@johnnys-mbp.lan>
2023-07-21 23:17:39 -0400

    countdown

--- Lunar Birthdays/ProfileView.swift
+++ Lunar Birthdays/ProfileView.swift
@@ -10,9 +10,30 @@ import SwiftUI
 struct ProfileView: View {
     var birthday: Birthday
     var body: some View {
-        Text(birthday.name)
+        VStack {
+            Image(birthday.picture)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 150)
+                .clipShape(Circle())
+            Text(birthday.name)
+                .font(.system(size: 40))
+                .fontWeight(.semibold)
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            Text(birthday.date)
+                .font(.system(size: 25))
+                .lineLimit(1)
+                .padding(.horizontal)
+                .foregroundColor(.secondary)
+            // Turning X in X Days
+            // Notes
+            // Custom Notifications
+        }
     }
 }
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
