3ec7517d940b68eaa62b5ac502bfbdca2451b21c
Johnny Jiang <johnnyjiang@johnnys-mbp.lan>
2023-07-23 01:59:49 -0400

    First commit

---
+++ LunarBirthdays/LunarBirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarBirthdaysApp.swift
+//  LunarBirthdays
+//
+//  Created by Johnny Jiang on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LunarBirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
