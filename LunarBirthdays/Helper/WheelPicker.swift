c3d3518b09df24d081648118561a3c5e389a424d
Johnny Jiang <johnnyjiang@johnnys-mbp.lan>
2023-07-30 13:12:18 -0400

    Wheel date picker

--- LunarBirthdays/Helper/WheelPicker.swift
+++ LunarBirthdays/Helper/WheelPicker.swift
@@ -8,10 +8,49 @@
 import SwiftUI
 
 struct WheelDatePicker: View {
+    @Binding var selectedDate: Date
+    @State private var isPickerVisible = false
     
     var body: some View {
-        VStack {
-            
+        HStack {
+            Text("Date")
+            Spacer()
+            HStack(spacing: 1) {
+                if let selectedDate = selectedDate {
+                    Text(selectedDate, style: .date)
+                        .foregroundColor(.gray)
+                } else {
+                    Text("Select Date")
+                        .foregroundColor(.gray)
+                }
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.gray)
+            }
+
+        }
+        .onTapGesture {
+            withAnimation {
+                isPickerVisible.toggle()
+            }
+        }
+        
+        if isPickerVisible {
+            VStack(spacing: 0) {
+                HStack(alignment: .center) {
+                    Button("Done") {
+                        withAnimation {
+                            isPickerVisible.toggle()
+                        }
+                    }
+                }
+                .padding(.top, 3)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.blue)
+                DatePicker("", selection: $selectedDate, in: ...Date(), displayedComponents: .date)
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .labelsHidden()
+                    .frame(maxHeight: 300)
+            }
         }
     }
 }
