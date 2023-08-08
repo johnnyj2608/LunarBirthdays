446b1ffc383a4e8933693d0f43df05ba4927dbe4
Johnny Jiang <johnnyj2608@gmail.com>
2023-08-07 23:45:03 -0400

    Reading Notification Permissions

--- LunarBirthdays/LunarBirthdaysApp.swift
+++ LunarBirthdays/LunarBirthdaysApp.swift
@@ -11,11 +11,37 @@ import SwiftUI
 struct LunarBirthdaysApp: App {
     @StateObject private var dataController = DataController()
     @AppStorage("darkMode") private var darkMode = true
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .preferredColorScheme(darkMode ? .dark : .light)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        registerForNotification()
+        return true
+    }
+    
+    func registerForNotification() {
+        //For device token and push notifications.
+        UIApplication.shared.registerForRemoteNotifications()
+        
+        let center : UNUserNotificationCenter = UNUserNotificationCenter.current()
+        //        center.delegate = self
+        
+        center.requestAuthorization(options: [.sound , .alert , .badge ], completionHandler: { (granted, error) in
+            if ((error != nil)) { UIApplication.shared.registerForRemoteNotifications() }
+            else {
+                
+            }
+        })
     }
 }
