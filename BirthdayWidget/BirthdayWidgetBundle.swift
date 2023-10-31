eb47363349d69ecc11082478cfb94a20e89c11cf
Johnny Jiang <johnnyj2608@gmail.com>
2023-10-30 23:54:52 -0400

    Widget Init

---
+++ BirthdayWidget/BirthdayWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  BirthdayWidgetBundle.swift
+//  BirthdayWidget
+//
+//  Created by Johnny Jiang on 10/30/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BirthdayWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BirthdayWidget()
+    }
+}
