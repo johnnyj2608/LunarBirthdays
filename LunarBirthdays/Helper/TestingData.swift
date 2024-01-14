c57638fd72319e5d6e16c2e69e4848f43d726711
Johnny Jiang <johnnyj2608@gmail.com>
2024-01-14 13:19:23 -0500

    Populated testing data with images

--- LunarBirthdays/Helper/TestingData.swift
+++ LunarBirthdays/Helper/TestingData.swift
@@ -11,14 +11,15 @@ import CoreData
 func populateCoreData(managedObjContext: NSManagedObjectContext) {
     
     DataController.shared.deleteAllBirthdays(context: managedObjContext)
+    DataController.shared.clearFileSystem()
     
     let calendar = Calendar.current
     let sampleData: [(img: UIImage, name: String, date: Date, note: String, lunar: Bool)] = [
-        (UIImage(named: "Logo") ?? UIImage(), "Asian Jim", calendar.date(from: DateComponents(year: 1978, month: 10, day: 1))!, "", true),
-        (UIImage(named: "Logo") ?? UIImage(), "Jim Halpert", calendar.date(from: DateComponents(year: 1978, month: 10, day: 1))!, "", false),
-        (UIImage(named: "Logo") ?? UIImage(), "Michael Scott", calendar.date(from: DateComponents(year: 1965, month: 3, day: 15))!, "", false),
-        (UIImage(named: "Logo") ?? UIImage(), "Pam Beesly", calendar.date(from: DateComponents(year: 1979, month: 3, day: 25))!, "", false),
-        (UIImage(named: "Logo") ?? UIImage(), "Dwight Schrute", calendar.date(from: DateComponents(year: 1970, month: 1, day: 20))!, "", false),
+        (UIImage(named: "AsianJim") ?? UIImage(), "Asian Jim", calendar.date(from: DateComponents(year: 1978, month: 10, day: 1))!, "", true),
+        (UIImage(named: "Jim") ?? UIImage(), "Jim Halpert", calendar.date(from: DateComponents(year: 1978, month: 10, day: 1))!, "", false),
+        (UIImage(named: "Michael") ?? UIImage(), "Michael Scott", calendar.date(from: DateComponents(year: 1965, month: 3, day: 15))!, "", false),
+        (UIImage(named: "Pam") ?? UIImage(), "Pam Beesly", calendar.date(from: DateComponents(year: 1979, month: 3, day: 25))!, "", false),
+        (UIImage(named: "Dwight") ?? UIImage(), "Dwight Schrute", calendar.date(from: DateComponents(year: 1970, month: 1, day: 20))!, "", false),
     ]
     
     for data in sampleData {
