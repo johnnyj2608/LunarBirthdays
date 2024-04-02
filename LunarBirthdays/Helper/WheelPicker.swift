6e84114a7f64a0fca4814cf751edd1b09c652da7
Johnny Jiang <johnnyj2608@gmail.com>
2024-04-01 23:39:35 -0400

    Fixed wheel picker

--- LunarBirthdays/Helper/WheelPicker.swift
+++ LunarBirthdays/Helper/WheelPicker.swift
@@ -11,6 +11,8 @@ struct WheelDatePicker: View {
     @Binding var selectedDate: Date
     @State private var isPickerVisible = false
     
+    let calendar = Calendar.current
+    
     private let start: Date = {
             var components = DateComponents()
             components.year = 1900
@@ -52,10 +54,31 @@ struct WheelDatePicker: View {
                 .padding(.top, 3)
                 .frame(maxWidth: .infinity)
                 .foregroundColor(.blue)
-                DatePicker("", selection: $selectedDate, in: start...Date(), displayedComponents: .date)
+                DatePicker("", selection: $selectedDate, displayedComponents: .date)
                     .datePickerStyle(WheelDatePickerStyle())
                     .labelsHidden()
                     .frame(maxHeight: 300)
+                    .onChange(of: selectedDate) { newValue in
+                        let today = calendar.startOfDay(for: Date())
+                        let selectedMonth = calendar.component(.month, from: selectedDate)
+                        let selectedDay = calendar.component(.day, from: selectedDate)
+                        
+                        if selectedDate > today {
+                            let newYear = calendar.component(.year, from: Date())
+                            var adjustedDate = calendar.date(from: DateComponents(year: newYear, month: selectedMonth, day: selectedDay))!
+                            
+                            if adjustedDate > today {
+                                adjustedDate = calendar.date(from: DateComponents(year: newYear - 1, month: selectedMonth, day: selectedDay))!
+                            }
+
+                            selectedDate = adjustedDate
+                        } else if selectedDate < calendar.date(from: DateComponents(year: 1900, month: 1, day: 1))! {
+                            let newYear = 1900
+                            
+                            selectedDate = calendar.date(from: DateComponents(year: newYear, month: selectedMonth, day: selectedDay))!
+                        }
+                    }
+                
             }
         }
     }
